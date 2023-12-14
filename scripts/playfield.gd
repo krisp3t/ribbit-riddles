268d481fe0805c7164966c8890d539beaa6bcc06
krisp3t <77499501+krisp3t@users.noreply.github.com>
2023-12-15 00:12:24 +0100

    Quick bugfix

--- scripts/playfield.gd
+++ scripts/playfield.gd
@@ -6,6 +6,9 @@ const DROP_SHORTEST_DIST = 75;
 var frog_scene : PackedScene = preload("res://scenes/frog.tscn");
 var lilypad_scene : PackedScene = preload("res://scenes/lilypad.tscn");
 var lilypads : Dictionary = {};
+var frogs_left : int = 0;
+
+signal solved;
 
 func _center_self() -> void:
 	position = get_viewport_rect().size / 2;
@@ -15,9 +18,9 @@ func _center_self() -> void:
 func _ready() -> void:
 	_center_self();
 	get_tree().get_root().connect("size_changed", _center_self);
-	var layout : Dictionary = read_JSON.get_dict("res://levels/easy.json");
+	var layout : Dictionary = read_JSON.get_dict("res://levels/veteran.json");
 	# TODO: get current level instead of hardcode	
-	var level_layout : Array = layout["1"];
+	var level_layout : Array = layout["40"];
 	
 	for y in level_layout.size():
 		for x in level_layout[y].size():
@@ -41,15 +44,13 @@ func _ready() -> void:
 			var frog : Node2D = frog_scene.instantiate();
 			frog.attached_lilypad = lilypad;
 			lilypad.attached_frog = frog;
+			frog.position = lilypad.position;
 			frog.red = (val == lilypad_enum.STATUS.RED);
 			frog.connect('drop_frog', _on_frog_drop);
 			add_child(frog);
+			frogs_left += 1;
 
-func _check_valid_move(frog: Frog, target_lilypad: Lilypad) -> bool:
-	if target_lilypad.attached_frog != null:
-		return false;
-	var start : Vector2i = frog.attached_lilypad.coord;
-	var target : Vector2i = target_lilypad.coord;
+func _get_between_lilypad(start: Vector2i, target: Vector2i) -> Lilypad:
 	var between : Vector2i;
 	# Horizontal move
 	if (start.x == target.x and abs(start.y - target.y) == 4):
@@ -59,33 +60,49 @@ func _check_valid_move(frog: Frog, target_lilypad: Lilypad) -> bool:
 		between = Vector2i(abs(start.x - target.x) / 2, start.y);
 	# Diagonal move
 	elif (abs(start.x - target.x) == 2 and abs(start.y - target.y) == 2):
-		between = Vector2i(min(start.x, target.x) + 1, min(start.x, target.x) + 1);
+		between = Vector2i(min(start.x, target.x) + 1, min(start.y, target.y) + 1);
 	else:
+		return null;
+	return lilypads[between];
+	
+func _check_valid_move(start: Lilypad, between: Lilypad, target: Lilypad) -> bool:
+	if (target.attached_frog != null || between == null):
 		return false;
-	print_debug("check valid move", start, target, between);
-	
-	var between_lilypad : Lilypad = lilypads[between];
-	if (between_lilypad.attached_frog == null || between_lilypad.attached_frog.red == true):
+	if (between.attached_frog == null || between.attached_frog.red == true):
 		return false;		
-	
-	# Jump over the in-between frog
-	between_lilypad.attached_frog.queue_free();
-	between_lilypad.attached_frog = null;
 	return true;
+
+func _check_level_solved() -> bool:
+	return frogs_left == 1;
 	
 	
 func _on_frog_drop(frog: Frog) -> void:
 	for child in get_tree().get_nodes_in_group("lilypads"):
 		var lilypad : Lilypad = child;
 		var distance : float = frog.position.distance_to(lilypad.position);
-		# Drop to the closest lilypad		
-		if distance < DROP_SHORTEST_DIST:
-			if (!_check_valid_move(frog, lilypad)):
-				break;
-			frog.attached_lilypad.attached_frog = null;
-			frog.attached_lilypad = lilypad;
-			lilypad.attached_frog = frog;
+		
+		if distance >= DROP_SHORTEST_DIST:
+			continue;
+		
+		# Drop to the lilypad close enough
+		var start : Vector2i = frog.attached_lilypad.coord;
+		var target : Vector2i = lilypad.coord;
+		var between : Lilypad = _get_between_lilypad(start, target);
+		if (!_check_valid_move(lilypads[start], between, lilypads[target])):
 			break;
+			
+		# Jump over frog
+		frogs_left -= 1;
+		between.attached_frog.queue_free();
+		between.attached_frog = null;
+		frog.attached_lilypad.attached_frog = null;
+		frog.attached_lilypad = lilypad;
+		lilypad.attached_frog = frog;
+		break;
+	
+	if _check_level_solved():
+		print_debug("win");
+		solved.emit();
 	
 
 
