4aee858a11de0873a6cb6dfcd1899492dd72ad09
krisp3t <77499501+krisp3t@users.noreply.github.com>
2023-12-14 20:34:39 +0100

    Make Playfield responsive

--- scenes/playfield.gd
+++ scenes/playfield.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const LILYPADS_OFFSET : Vector2i = Vector2i(200, 100);
+const DROP_SHORTEST_DIST = 75;
 
 var frog_scene : PackedScene = preload("res://scenes/frog.tscn");
 var lilypad_scene : PackedScene = preload("res://scenes/lilypad.tscn");
@@ -10,9 +11,15 @@ func read_JSON(json_file_path: String) -> Dictionary:
 	var file : FileAccess = FileAccess.open(json_file_path, FileAccess.READ);
 	var content : String = file.get_as_text();
 	return JSON.new().parse_string(content);
+
+func center_self() -> void:
+	position = get_viewport_rect().size / 2;
+	print_debug(position);
 	
 
 func _ready() -> void:
+	center_self();
+	get_tree().get_root().connect("size_changed", center_self);
 	var layout : Dictionary = read_JSON("res://levels/easy.json");
 	# TODO: get current level instead of hardcode	
 	var level_layout : Array = layout["1"];
@@ -23,21 +30,31 @@ func _ready() -> void:
 			
 			# Instantiate lilypads
 			var lilypad : Node2D = lilypad_scene.instantiate();
+			lilypad.coord = Vector2i(x, y);
 			if (y % 2 == 0):
-				lilypad.position = Vector2i(x * LILYPADS_OFFSET.x, y * LILYPADS_OFFSET.y);
+				lilypad.position = Vector2i((x - 1) * LILYPADS_OFFSET.x, (y - 1) * LILYPADS_OFFSET.y);
 			else:
 				# Centre the rows with even numbers of lilypads (have 1 lilypad less)
-				lilypad.position = Vector2i(x * LILYPADS_OFFSET.x + (LILYPADS_OFFSET.x / 2), y * LILYPADS_OFFSET.y);
+				lilypad.position = Vector2i((x - 1) * LILYPADS_OFFSET.x + (LILYPADS_OFFSET.x / 2), (y - 1) * LILYPADS_OFFSET.y);
 			add_child(lilypad);
 			if (val == 0): 
 				continue;
 			
 			# Instantiate frogs
 			var frog : Node2D = frog_scene.instantiate();
+			frog.coord = Vector2i(x, y);
 			frog.red = (val == 2);
-			frog.rest_point = lilypad.global_position;
+			frog.rest_point = lilypad.position;
+			frog.connect('drop_frog', _on_frog_drop);
 			add_child(frog);
-			
+
+func _on_frog_drop(frog: Frog) -> void:
+	for child in get_tree().get_nodes_in_group("lilypads"):
+		# Drop to the closest lilypad
+		var distance : float = frog.position.distance_to(child.position);
+		if distance < DROP_SHORTEST_DIST:
+			print_debug("frog dropping", child.coord);
+			frog.rest_point = child.position;
 
 
 func _process(delta: float) -> void:
--- scripts/lilypad.gd
+++ scripts/lilypad.gd
@@ -1,9 +1,5 @@
 extends Marker2D
-	
-func select():
-	for child in get_tree().get_nodes_in_group("zone"):
-		child.deselect();
-	modulate = Color.TRANSPARENT;
-	
-func deselect():
-	modulate = Color.WHITE;
+@export var coord: Vector2i;
+
+func _ready():
+	add_to_group("lilypads");
