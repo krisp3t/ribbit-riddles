6780e547b78b84ece79ab9475c618ef2704bd2f5
krisp3t <77499501+krisp3t@users.noreply.github.com>
2023-12-28 03:00:36 +0100

    Add lines between lilypads

--- scripts/playfield.gd
+++ scripts/playfield.gd
@@ -6,7 +6,7 @@ const DROP_SHORTEST_DIST : int = 75;
 
 var frog_scene : PackedScene = preload("res://scenes/frog.tscn");
 var lilypad_scene : PackedScene = preload("res://scenes/lilypad.tscn");
-var lilypads : Dictionary = {};
+var lilypads : Array = [];
 var frogs_left : int = 0;
 
 signal solved;
@@ -34,14 +34,37 @@ func _get_center_position() -> Vector2:
 func _on_viewport_size_changed() -> void:
 	position = _get_center_position();
 	
-func _draw_lines_between_lilypads(dict : Dictionary) -> void:
-	for pos in dict:
-		var lilypad : Lilypad = dict[pos];
-		print_debug(lilypad.coord);
+func _draw_lines_between_lilypads(arr : Array) -> void:
+	for x in arr:
+		# Odd rows
+		if len(x) % 2 != 0:
+			var l : Line2D = Line2D.new();
+			for lilypad in x:
+				l.add_point(lilypad.position);
+				# Connect upper and lower lilypad vertically
+				if lilypad.coord.x % 4 == 2:
+					var li: Line2D = Line2D.new();
+					var c : Vector2i = lilypad.coord;
+					var to_draw : Array = [lilypad.coord, Vector2i(c.x - 2, c.y), Vector2i(c.x + 2, c.y)];
+					for t in to_draw:
+						li.add_point(_get_lilypad(t).position);
+					add_child(li);
+			add_child(l);
+		else:
+			for lilypad in x:
+				var c : Vector2i = lilypad.coord;
+				var to_draw : Array = [Vector2i(c.x - 1, c.y - 1), Vector2i(c.x - 1, c.y + 1), Vector2i(c.x + 1, c.y - 1), Vector2i(c.x + 1, c.y + 1)];
+				for t in to_draw:
+					var l : Line2D = Line2D.new();
+					l.add_point(lilypad.position);
+					l.add_point(_get_lilypad(t).position);
+					add_child(l);
+				
 		
 func _instantiate_lilypads() -> void:
 	var level_layout : Array = level_vars.level_layout;	
 	for y in level_layout.size():
+		lilypads.push_back([]);
 		for x in level_layout[y].size():
 			var val : int = level_layout[y][x];
 			
@@ -55,7 +78,7 @@ func _instantiate_lilypads() -> void:
 				# Centre the rows (have 1 lilypad less)				
 				lilypad.position = Vector2(200 + x * LILYPADS_OFFSET.x + (LILYPADS_OFFSET.x / 2.0), 150 + y * LILYPADS_OFFSET.y);
 			add_child(lilypad);
-			lilypads[lilypad.coord] = lilypad;
+			lilypads[y].push_back(lilypad);
 			
 			# Empty lilypads
 			if (val == lilypad_enum.STATUS.EMPTY): 
@@ -70,6 +93,14 @@ func _instantiate_lilypads() -> void:
 			frog.connect('drop_frog', _on_frog_drop);
 			add_child(frog);
 			frogs_left += 1;
+			
+func _get_lilypad(coord: Vector2i) -> Lilypad:
+	var y : int;
+	if (coord.x % 2 == 0):
+		y = coord.y / 2;
+	else:
+		y = (coord.y - 1) / 2;
+	return lilypads[coord.x][y];
 	
 func _get_between_lilypad(start: Vector2i, target: Vector2i) -> Lilypad:
 	var between : Vector2i;
@@ -84,7 +115,7 @@ func _get_between_lilypad(start: Vector2i, target: Vector2i) -> Lilypad:
 		between = Vector2i(min(start.x, target.x) + 1, min(start.y, target.y) + 1);
 	else:
 		return null;
-	return lilypads[between];
+	return _get_lilypad(between);
 	
 func _check_valid_move(between: Lilypad, target: Lilypad) -> bool:
 	if (target.attached_frog != null || between == null):
@@ -108,7 +139,7 @@ func _on_frog_drop(frog: Frog) -> void:
 		var start : Vector2i = frog.attached_lilypad.coord;
 		var target : Vector2i = lilypad.coord;
 		var between : Lilypad = _get_between_lilypad(start, target);
-		if (!_check_valid_move(between, lilypads[target])):
+		if (!_check_valid_move(between, _get_lilypad(target))):
 			break;
 			
 		# Jump over frog
