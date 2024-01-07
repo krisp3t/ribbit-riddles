be28be7a4ca39c13f15b69ea8a833e2728f59fb4
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-07 02:33:18 +0100

    Fix clicking on sliders in options

--- scripts/sidebars/options.gd
+++ scripts/sidebars/options.gd
@@ -10,23 +10,31 @@ func _ready() -> void:
 	%SoundtrackHSlider.value = config.load_value("audio", "bg") if config.load_value("audio", "bg") != null else 100.0;
 	%SoundtrackProgressBar.value = %SoundtrackHSlider.value;
 
+### Soundtrack
 func _on_soundtrack_h_slider_value_changed(value: float) -> void:
 	%SoundtrackProgressBar.value = %SoundtrackHSlider.value;	
 
-func _on_soundtrack_h_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		%SoundtrackProgressBar.value = %SoundtrackHSlider.value;	
-		config.save_value("audio", "bg", %SoundtrackHSlider.value);
+func _on_soundtrack_h_slider_drag_ended(_value_changed: bool) -> void:
+	%SoundtrackProgressBar.value = %SoundtrackHSlider.value;	
+	config.save_value("audio", "bg", %SoundtrackHSlider.value);
 	audio_bg_change.emit();
-	
+
+func _on_soundtrack_h_slider_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		_on_soundtrack_h_slider_drag_ended(true);
+
+### SFX	
 func _on_sfxh_slider_value_changed(value: float) -> void:
 	%SFXProgressBar.value = %SFXHSlider.value;	
 
-func _on_sfxh_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		%SFXProgressBar.value = %SFXHSlider.value;	
-		config.save_value("audio", "sfx", %SFXHSlider.value);
-			
+func _on_sfxh_slider_drag_ended(_value_changed: bool) -> void:
+	%SFXProgressBar.value = %SFXHSlider.value;	
+	config.save_value("audio", "sfx", %SFXHSlider.value);
+	
+func _on_sfxh_slider_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		_on_sfxh_slider_drag_ended(true);
+	
 func _on_reset_button_pressed() -> void:
 	if (DirAccess.dir_exists_absolute("user://savegames")):
 		var dir : DirAccess = DirAccess.open("user://savegames");
@@ -35,3 +43,6 @@ func _on_reset_button_pressed() -> void:
 	get_tree().reload_current_scene();
 	%ResetButton.disabled = true;
 	%ResetLabel.text = "Successfully reset game!";
+
+
+
