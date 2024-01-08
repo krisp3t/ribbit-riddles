bce4cce5bcec5079a9e2b2f57b57bd619be2f5f9
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-08 20:26:49 +0100

    Improve Menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -63,5 +63,5 @@ func _on_credits_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit();
 
-
-
+func _on_tutorial_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/tutorial_level.tscn");
