013fade0932386c48332de5881abbe165d5ec208
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-08-20 03:20:36 +0200

    Simplify main menu / sidebar

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,63 +2,51 @@ extends Control
 @onready var level_vars : LevelSystem = $/root/LevelSystem;
 @onready var config : Config = $/root/ConfigSystem;
 
-var is_sidebar_open : bool = false;
+var sidebar_is_open : bool = true;
 
 func _ready() -> void:
-	_change_sidebar_state(false, false);
-	_close_all_sidebars();
+	_toggle_sidebar(false, false);
 	%SoundtrackPlayer.volume_db = audio_system.get_db(config.load_value("audio", "bg", 100.0));
 	
 func _on_options_audio_bg_change() -> void:
 	%SoundtrackPlayer.volume_db = audio_system.get_db(config.load_value("audio", "bg", 100.0));
 
-func _change_sidebar_state(open: bool, animate: bool = true) -> void:
-	var sidebar_node : Control = $Sidebar;
-	var pos : Vector2 = Vector2(sidebar_node.position.x, sidebar_node.position.y);
-	if open:
-		pos.x = sidebar_node.position.x - sidebar_node.size.x;
-	else:
-		pos.x = sidebar_node.position.x + sidebar_node.size.x;
+func _toggle_sidebar(will_open: bool, animate: bool = true, node: Control = null) -> void:
+	%Difficulty.visible = false;
+	%Options.visible = false;
+	%Credits.visible = false;
+	if node:
+		node.visible = true;
+		%SidebarLabel.text = node.name;
+	var sidebar : Control = %Sidebar;
+	var pos : Vector2 = Vector2(sidebar.position.x, sidebar.position.y);
+	if will_open and !sidebar_is_open:
+		pos.x = sidebar.position.x - sidebar.size.x;
+	elif !will_open and sidebar_is_open:
+		pos.x = sidebar.position.x + sidebar.size.x;
 	if animate:
 		var tween : Tween = get_tree().create_tween();
 		tween.set_trans(Tween.TRANS_QUAD);
-		tween.tween_property(sidebar_node, "position", pos, 0.5);
+		tween.tween_property(sidebar, "position", pos, 0.5);
 	else:
-		sidebar_node.position = pos;
-	is_sidebar_open = open;
-	
-func _close_all_sidebars() -> void:
-	%DifficultySelector.visible = false;
-	%Options.visible = false;
-	%Credits.visible = false;
-	
-func _on_play_pressed() -> void:
-	_close_all_sidebars();
-	%DifficultySelector.visible = true;
-	%SidebarLabel.text = "Difficulty";
-	if !is_sidebar_open:
-		_change_sidebar_state(true);
+		sidebar.position = pos;
+	sidebar_is_open = will_open;
 
 func _on_close_sidebar_button_pressed() -> void:
-	_change_sidebar_state(false);
+	_toggle_sidebar(false);
 	
+func _on_play_pressed() -> void:
+	_toggle_sidebar(true, true, %Difficulty);
+
 func _on_options_pressed() -> void:
-	_close_all_sidebars();
-	%Options.visible = true;
-	%SidebarLabel.text = "Options";
-	if !is_sidebar_open:
-		_change_sidebar_state(true);
+	_toggle_sidebar(true, true, %Options);
+		
+func _on_credits_pressed() -> void:
+	_toggle_sidebar(true, true, %Credits);
 		
 func _on_map_editor_pressed() -> void:
 	level_vars.initialize(level_enum.MAX_EXPERT + 1);
 	get_tree().change_scene_to_file("res://scenes/editor.tscn");
-	
-func _on_credits_pressed() -> void:
-	_close_all_sidebars();
-	%Credits.visible = true;
-	%SidebarLabel.text = "Credits";
-	if !is_sidebar_open:
-		_change_sidebar_state(true);
 	
 func _on_exit_pressed() -> void:
 	get_tree().quit();
